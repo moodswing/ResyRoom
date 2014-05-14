eca6364eb8ef85cf0a1ab809bdd769f289d093a5
moodswing <rob.arav@gmail.com>
2014-05-14 09:14:06 -0700

    new files bitches (breaking bad)

---
+++ ResyRoom/ViewModels/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ResyRoom.Models;
+
+namespace ResyRoom.ViewModels
+{
+    public abstract class BaseViewModel
+    {
+        public IdentificacionDeUsuario Usuario { get; set; }
+    }
+}
